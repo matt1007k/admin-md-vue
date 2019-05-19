cf39ecf01c84b864aa898bb3ede1acab5b0e429c
matt1007k <matt1007k@gmail.com>
2019-05-18 23:06:18 -0400

    Configurando multiples layouts

---
+++ src/components/layout/LayoutAdmin.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <v-toolbar dark color="primary">
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+
+      <v-toolbar-title class="white--text">Admin</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>$vuetify.icons.search</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>$vuetify.icons.apps</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "LayoutAdmin"
+};
+</script>
+
+<style>
+</style>
---
+++ src/plugins/mdi-icons.js
@@ -0,0 +1,12 @@
+const mdi_icons = {
+  apps: "mdi-dropbox",
+  support: "mdi-lifebuoy",
+  steam: "mdi-steam-box",
+  pc: "mdi-desktop-classic",
+  xbox: "mdi-xbox",
+  playstation: "mdi-playstation",
+  switch: "mdi-nintendo-switch",
+  search: "mdi-magnify"
+};
+
+export default mdi_icons;
